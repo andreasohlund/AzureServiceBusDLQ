b9b5b533eca806a6c9566dd5a3743e78232fb374
Andreas Öhlund <andreas.ohlund@particular.net>
2025-03-16 21:08:43 +0100

    Check subscriptions as well

--- AzureServiceBusDLQ/StatusCommand.cs
+++ AzureServiceBusDLQ/StatusCommand.cs
@@ -22,44 +22,82 @@ public class StatusCommand : CancellableAsyncCommand<StatusCommand.Settings>
 
     public override async Task<int> ExecuteAsync(CommandContext context, Settings settings, CancellationToken cancellationToken)
     {
-        var table = new Table
-        {
-            Title = new TableTitle("DLQ Status")
-        };
-
-        table.AddColumn("Queue");
-        table.AddColumn(new TableColumn("Count").Centered());
-
-        var queues = new List<QueueProperties>();
-
-        //TODO: Spinner
-        await foreach (var queue in settings.AdministrationClient.GetQueuesAsync(cancellationToken))
-        {
-            queues.Add(queue);
-        }
-
         await AnsiConsole.Progress()
-            .Columns(new TaskDescriptionColumn(), new ProgressBarColumn(), new PercentageColumn(), new RemainingTimeColumn(), new SpinnerColumn())
+            .Columns(new TaskDescriptionColumn(), new SpinnerColumn())
+            .HideCompleted(true)
             .StartAsync(async ctx =>
             {
-                var task = ctx.AddTask($"Fetching DLQ status for {queues.Count} queues", new ProgressTaskSettings());
+                var queuesProgress = ctx.AddTask($"Fetching queue DLQ status");
+                var subscriptionProgress = ctx.AddTask($"Fetching subscription DLQ status");
 
-                task.MaxValue = queues.Count;
-                await Task.WhenAll(queues.Select(async queue =>
+                var queues =  await GetQueueRuntimeProperties(settings.AdministrationClient, queuesProgress, cancellationToken);
+                var subscriptions =  await GetSubscriptionRuntimeProperties(settings.AdministrationClient, subscriptionProgress, cancellationToken);
+
+                var queueTable = new Table
                 {
-                    var response = await settings.AdministrationClient.GetQueueRuntimePropertiesAsync(queue.Name, cancellationToken);
-                    if (!response.HasValue)
-                    {
-                        throw new InvalidOperationException($"Failed to get queue runtime properties for {queue.Name}");
-                    }
+                    Title = new TableTitle("Queue DLQ Status")
+                };
 
-                    task.Increment(1);
-                    table.AddRow(queue.Name, response.Value.DeadLetterMessageCount.ToString());
-                }));
+                queueTable.AddColumn("Queue");
+                queueTable.AddColumn(new TableColumn("Count").Centered());
+                
+                foreach (var queue in queues)
+                {
+                    queueTable.AddRow(queue.Name, $"{queue.DeadLetterMessageCount.ToString()}:{queue.TransferDeadLetterMessageCount.ToString()}");
+                }
+                
+                var subscriptionTable = new Table
+                {
+                    Title = new TableTitle("Subscription DLQ Status")
+                };
+
+                subscriptionTable.AddColumn("Subscription");
+                subscriptionTable.AddColumn(new TableColumn("Count").Centered());
+                
+                foreach (var subscription in subscriptions)
+                {
+                    subscriptionTable.AddRow(subscription.SubscriptionName, $"{subscription.DeadLetterMessageCount.ToString()}:{subscription.TransferDeadLetterMessageCount.ToString()}");
+                }
+                
+                AnsiConsole.Write(queueTable);
+                AnsiConsole.Write(subscriptionTable);
             });
 
-        AnsiConsole.Write(table);
+     
 
         return 0;
+    }
+
+    async Task<List<QueueRuntimeProperties>> GetQueueRuntimeProperties(ServiceBusAdministrationClient client, ProgressTask progressTask, CancellationToken cancellationToken)
+    {
+        var queues = new List<QueueRuntimeProperties>();
+
+        await foreach (var queue in client.GetQueuesRuntimePropertiesAsync(cancellationToken))
+        {
+            queues.Add(queue);
+            progressTask.Increment(1);
+        }
+
+        progressTask.StopTask();
+
+        return queues;
+    }
+    
+    async Task<List<SubscriptionRuntimeProperties>> GetSubscriptionRuntimeProperties(ServiceBusAdministrationClient client, ProgressTask progressTask, CancellationToken cancellationToken)
+    {
+        var subscriptions = new List<SubscriptionRuntimeProperties>();
+
+        await foreach (var topic in client.GetTopicsAsync(cancellationToken))
+        {
+            await foreach (var queue in client.GetSubscriptionsRuntimePropertiesAsync(topic.Name,cancellationToken))
+            {
+                subscriptions.Add(queue);
+                progressTask.Increment(1);
+            }
+        }
+        
+        progressTask.StopTask();
+
+        return subscriptions;
     }
 }
